ce12459695b38561c274848c8f066912ab64a343
Karen Yazbaryan <karen.yazbaryan@gmail.com>
2018-05-16 22:10:18 +0400

    Main Page (empty) + Upload modal, facebook upload (from request) function, problems with file multipart

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 require('dotenv').config()
 
 const express = require('express')
+			bodyParser = require('body-parser')
+			path = require('path')
 			app = new express()
 			port = process.env.PORT || 3000;
 
-app.get('/', function(req, res) {
-	res.send('Hey');
-})
+app.use(bodyParser.urlencoded({
+	extended: true,
+	limit: '50mb'
+}))
 
+app.use(bodyParser.json({
+	limit: '50mb'
+}))
+
+app.use(express.static(__dirname + '/public'));
 app.use('/social', require('./routes/social'))
+
+app.get('/', function(req, res) {
+	res.sendFile(path.join(__dirname + '/public/index.html'));
+})
 
 app.listen(port, function () {
 	console.log(`Listening on port ${port}!`)
--- routes/social.js
+++ routes/social.js
@@ -1,15 +1,22 @@
 const express = require('express'),
 			router = express.Router(),
+			multer  = require('multer')
+			upload = multer()
 			social = require('../lib/social')
 
-router.get('/upload', function(req, res) {
-	social.upload().then(data => {
+router.post('/upload', upload.fields([{name: 'image'}]), function(req, res) {
+	social.upload(req)
+	.then(data => {
 		res.send(data)
+	})
+	.catch(err => {
+		res.status(500).send(err)
 	})
 })
 
 router.get('/info', function(req, res) {
-	social.getData().then(data => {
+	social.getData()
+	.then(data => {
 		res.send(data)
 	})
 })
